0d367bede3d865d26ca21ca0e265834777c0f0a5
Alan <alanrps>
2022-01-22 15:20:09 -0300

    Fix

--- src/utils/convertToSnakeCase.js
+++ src/utils/convertToSnakeCase.js
@@ -3,7 +3,7 @@ import { snakeCase } from "snake-case";
 export function convertToSnakeCase(params){
     const entriesParams = Object.entries(params);
 
-    const snakeCaseParams = entriesParams.reduce(acc, ([key, value]) => {
+    const snakeCaseParams = entriesParams.reduce((acc, [key, value]) => {
         acc[snakeCase(key)] = value;
 
         return acc;
